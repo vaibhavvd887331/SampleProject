4b4dcb8ec2eb2de7cf478fdd9ddcd53127ccea57
vaibhavvd887331 <vaibhavvd887331@gmail.com>
2025-09-14 11:11:06 +0530

    Delete CRUDConsole directory

--- CRUDConsole/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DataAccessLayer;
-
-Console.WriteLine("Hello, World!");
-
-ProductDAL _productDAL = new ProductDAL();
-
-Console.WriteLine(_productDAL.GetProducts());
